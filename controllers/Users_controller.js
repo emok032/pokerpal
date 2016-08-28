626927cd91540dd6174f4789056d6a985c5cf40e
sidjayisaok <sidjay18@gmail.com>
2016-08-27 22:00:33 -0400

    need to work on page redirect

--- controllers/Users_controller.js
+++ controllers/Users_controller.js
@@ -3,8 +3,7 @@ var bcrypt = require('bcryptjs');
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
-var path = require('path'); 
-
+var path = require('path');
 
 //this is the users_controller.js file
 router.get('/', function(req, res){
@@ -18,102 +17,182 @@ router.get('/sign-in', function(req,res) {
 	res.render('users/sign_in');
 });
 
+
+
 // Register (New) User
-router.post('/create', function(req,res) {
-	models.User.findAll({
-    where: {email: req.body.username}
-  }).then(function(users) {
+// router.post('/create', function(req,res) {
+// 	models.User.findAll({
+//     where: {email: req.body.username}
+//   }).then(function(users) {
+//
+// 		if (users.length > 0){
+// 			console.log(users)
+// 			res.send('An account already exists for this username')
+// 		}else{
+//
+// 			// Solution:
+// 			// =========
+//
+// 			// Using bcrypt, generate a 10-round salt,
+// 			// then use that salt to hash the user's password.
+// 			bcrypt.genSalt(10, function(err, salt) {
+// 					bcrypt.hash(req.body.password, salt, function(err, hash) {
+//
+// 						// Using the User model, create a new user,
+// 						// storing the email they sent and the hash you just made
+// 						models.User.create({
+// 							username: req.body.username,
+// 							password_hash: hash
+// 						})
+// 						// In a .then promise connected to that create method,
+// 						// save the user's information to req.session
+// 						// as shown in these comments
+// 						.then(function(user){
+//
+//
+// 							// so what's happening here?
+// 							// we enter the user's session by setting properties to req.
+//
+// 							// we save the logged in status to the session
+// 		          req.session.logged_in = true;
+// 		          // the username to the session
+// 							req.session.username = user.username;
+// 							// the user id to the session
+// 		          req.session.user_id = user.id;
+// 		          // and the user's email.
+// 		          req.session.user_email = user.email;
+//
+// 		          // redirect to home on login
+// 							res.redirect('/')
+// 						});
+// 					});
+// 			});
+//
+// 		}
+// 	});
+// });
+//
+// // Sign-In (Existing) User
+// router.post('/login', function(req, res) {
+//   models.User.findOne({
+//     where: {email: req.body.email}
+//   }).then(function(user) {
+//
+// 		if (user == null){
+// 			res.redirect('/')
+// 		}
+//
+// 		// Solution:
+// 		// =========
+// 		// Use bcrypt to compare the user's password input
+// 		// with the hash stored in the user's row.
+// 		// If the result is true,
+//     bcrypt.compare(req.body.password, user.password_hash, function(err, result) {
+//         // if the result is true (and thus pass and hash match)
+//         if (result == true){
+//
+//         	// save the user's information
+// 					// to req.session, as the comments below show
+//
+// 					// so what's happening here?
+// 					// we enter the user's session by setting properties to req.
+//
+// 					// we save the logged in status to the session
+//           req.session.logged_in = true;
+//           // the username to the session
+// 					req.session.username = user.username;
+// 					// the user id to the session
+//           req.session.user_id = user.id;
+//           // and the user's email.
+//           req.session.user_email = user.email;
+//
+//           res.redirect('/welcome');
+//         }
+//         // if the result is anything but true (password invalid)
+//         else{
+//         	// redirect user to sign in
+// 					res.redirect('/users/sign-in')
+// 				}
+//     });
+//   })
+// });
 
-		if (users.length > 0){
-			console.log(users)
-			res.send('An account already exists for this username')
-		}else{
-
-			// Solution:
-			// =========
-
-			// Using bcrypt, generate a 10-round salt,
-			// then use that salt to hash the user's password.
-			bcrypt.genSalt(10, function(err, salt) {
-					bcrypt.hash(req.body.password, salt, function(err, hash) {
-
-						// Using the User model, create a new user,
-						// storing the email they sent and the hash you just made
-						models.User.create({
-							username: req.body.username,
-							password_hash: hash
-						})
-						// In a .then promise connected to that create method,
-						// save the user's information to req.session
-						// as shown in these comments
-						.then(function(user){
-
-
-							// so what's happening here?
-							// we enter the user's session by setting properties to req.
-
-							// we save the logged in status to the session
-		          req.session.logged_in = true;
-		          // the username to the session
-							req.session.username = user.username;
-							// the user id to the session
-		          req.session.user_id = user.id;
-		          // and the user's email.
-		          req.session.user_email = user.email;
-
-		          // redirect to home on login
-							res.redirect('/')
-						});
-					});
-			});
-
-		}
-	});
-});
-
-// Sign-In (Existing) User
-router.post('/login', function(req, res) {
-  models.User.findOne({
-    where: {email: req.body.email}
-  }).then(function(user) {
-
-		if (user == null){
-			res.redirect('/')
-		}
-
-		// Solution:
-		// =========
-		// Use bcrypt to compare the user's password input
-		// with the hash stored in the user's row.
-		// If the result is true,
-    bcrypt.compare(req.body.password, user.password_hash, function(err, result) {
-        // if the result is true (and thus pass and hash match)
-        if (result == true){
-
-        	// save the user's information
-					// to req.session, as the comments below show
-
-					// so what's happening here?
-					// we enter the user's session by setting properties to req.
-
-					// we save the logged in status to the session
-          req.session.logged_in = true;
-          // the username to the session
-					req.session.username = user.username;
-					// the user id to the session
-          req.session.user_id = user.id;
-          // and the user's email.
-          req.session.user_email = user.email;
-
-          res.redirect('/welcome');
-        }
-        // if the result is anything but true (password invalid)
-        else{
-        	// redirect user to sign in
-					res.redirect('/users/sign-in')
-				}
+//passport implementation
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    models.User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
     });
-  })
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+//authentication request for passport
+router.post("/login",
+  passport.authenticate('local', {failureRedirect: '/login'}),
+  function(req, res){
+    res.redirect('/');
+  });
+
+
+	router.post("/register", function(req, res){
+	  models.User.create({
+	      username: req.body.username,
+	      password: req.body.password,
+	      email: req.body.email
+	    }).then(res.redirect('/home', function(error){
+				console.log(error)
+			}))
+	});
+// 	.then(function(user){
+//
+//
+// 		// so what's happening here?
+// 		// we enter the user's session by setting properties to req.
+//
+// 		// we save the logged in status to the session
+// 		// req.session.logged_in = true;
+// 		// // the username to the session
+// 		// req.session.username = user.username;
+// 		// // the user id to the session
+// 		// req.session.user_id = user.id;
+// 		// // and the user's email.
+// 		// req.session.user_email = user.email;
+//
+// 		// redirect to home on login
+// 		res.redirect('/')
+// 	});
+// });;
+// .then(function(user){
+//
+// 	// so what's happening here?
+// 	// we enter the user's session by setting properties to req.
+//
+// 	// we save the logged in status to the session
+// 	req.session.logged_in = true;
+// 	// the username to the session
+// 	req.session.username = user.username;
+// 	// the user id to the session
+// 	req.session.user_id = user.id;
+// 	// and the user's email.
+// 	req.session.user_email = user.email;
+//
+// 	// redirect to home on login
+// 	res.redirect('/')
+// });
+// });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,18 +5,25 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var exphbs  = require('express-handlebars');
 var models = require('./models');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+var router = express.Router();
+
+//MIDDLEWARE//
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+app.use(require('express-session')({secret: 'keyboard dog', resave: true, saveUninitialized: true}));
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(express.static(process.cwd() + '/public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 // =============================================================
@@ -33,6 +40,54 @@ app.get('/', function (req, res) {
 
 app.get("/home", function(req, res){
   res.render("home")
+});
+
+app.get("/home/games", function(req, res){
+  models.game.findAll({
+    include: [models.User]
+  })
+  .then(function(games){
+    res.render('')
+  })
+})
+
+// //passport implementation
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     models.User.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) { return done(null, false); }
+//       if (!user.verifyPassword(password)) { return done(null, false); }
+//       return done(null, user);
+//     });
+//   }
+// ));
+//
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
+//
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
+//
+// //authentication request for passport
+// app.post("/login",
+//   passport.authenticate('local', {failureRedirect: '/login'}),
+//   function(req, res){
+//     res.redirect('/');
+//   });
+//
+app.post("/register", function(req, res){
+  models.User.create(
+    {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email
+    }
+  );
 });
 
 //Defining middleware to serve static files
